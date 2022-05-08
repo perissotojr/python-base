0ab29508f1d5774ee3d6ee789e15fe1d832ad126
Junior Perissoto <junior.perissoto@gmail.com>
2022-05-08 18:39:26 -0300

    Adicionado novas funções ao scritp.

--- hello.py
+++ hello.py
@@ -1,7 +1,35 @@
 #!/usr/bin/env python3
+"""Hello World Multi Linguas.
 
-#Este programa imprime Hello World
-print("Hello, World!!")
+Dependendo da lingua configurada no ambiente o programa exibe a mensagem
+correspondente.
 
-#Esse é uma soma entre dois numeros:
-print(50+50)
+Como usar:
+
+Tenha a variável LANG devidamente configurada ex:
+	export LANG=pt_BR
+
+Execução:
+
+	python3 hello.py
+	ou
+	./hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "Junior Perissoto"
+__license__ = "Unlicense"
+
+import os
+
+
+current_language = os.getenv("LANG")
+
+msg = "Hello,World!!!"
+
+if current_language == "pt_BR":
+	msg =  "Olá, Mundo!"
+elif current_language == "it_IT":
+	msg= "Ciao, Mondo!!!"
+
+
+print(msg)
