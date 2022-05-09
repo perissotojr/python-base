e8b2c5de0224b56d73c68a44bb32e6d837b1ca6c
Junior Perissoto <junior.perissoto@gmail.com>
2022-05-09 16:14:26 -0300

    Adicionado gitigore

--- hello.py
+++ hello.py
@@ -16,7 +16,7 @@ Execução:
 	./hello.py
 """
 __version__ = "0.0.1"
-__author__ = "Junior Perissoto"
+__author__ = "Junior Perissoto" 
 __license__ = "Unlicense"
 
 import os
@@ -27,7 +27,7 @@ current_language = os.getenv("LANG" , "en_US")[:5]
 msg = "Hello,World!!!"
 
 if current_language == "pt_BR":
-	msg =  "Olá, Mundo!"
+    msg =  "Olá, Mundo!"
 elif current_language == "it_IT":
 	msg= "Ciao, Mondo!!!"
 
