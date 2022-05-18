234c5ac1484111aa71e76b9b4642c3d0775a7541
Junior Perissoto <junior.perissoto@gmail.com>
2022-05-18 18:37:12 -0300

    Adicionado mais alguns scritps e melhorado outros usando dicionario:

--- hello.py
+++ hello.py
@@ -15,21 +15,23 @@ Execução:
 	ou
 	./hello.py
 """
-__version__ = "0.0.1"
+__version__ = "0.1.2"
 __author__ = "Junior Perissoto" 
 __license__ = "Unlicense"
 
+#sets (Hash Table) - 0(1) - constante
+#dicts (hash table)
 import os
-
 
 current_language = os.getenv("LANG" , "en_US")[:5]
 
-msg = "Hello,World!!!"
+msg = {
+	"en_US": "Hello, World!",
+	"pt_BR": "Olá, Mundo!",
+	"it_IT": "Ciao, Mondo!",
+	"es_SP": "Hola, Mundo!",
+	"fr_FR": "Bonjour Monde",
+}
 
-if current_language == "pt_BR":
-    msg =  "Olá, Mundo!"
-elif current_language == "it_IT":
-	msg= "Ciao, Mondo!!!"
-
-
-print(msg)
+#0(1) 
+print(msg[current_language])
